195d211f3fe181cad83571971ddbeaa706dccf1d
jacknson <aslimani48@gmail.com>
2020-12-10 12:01:42 +0000

    4.7

--- library/src/main/java/com/scanlibrary/ProgressDialogFragment.java
+++ library/src/main/java/com/scanlibrary/ProgressDialogFragment.java
@@ -24,6 +24,7 @@ public class ProgressDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final ProgressDialog dialog = new ProgressDialog(getActivity(), R.style.AlertDialogCustom);
+
         Window win = dialog.getWindow();
         win.setGravity(Gravity.CENTER);
         dialog.setIndeterminate(true);
